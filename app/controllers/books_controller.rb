8f52d682dc048018c2470c029c6a01041f7029ee
momose1031 <macbookair2020momose@icloud.com>
2021-04-27 11:44:37 +0000

    index layout form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,29 @@
 class BooksController < ApplicationController
   def index
+    
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
+    
   end
 
   def edit
+
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
