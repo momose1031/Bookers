0676350bbea1fbf53f689b82b1875e6889c63090
momose1031 <macbookair2020momose@icloud.com>
2021-04-28 02:34:40 +0000

    [finish]all layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path
   end
 
   def show
@@ -19,10 +19,16 @@ class BooksController < ApplicationController
   end
   
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
   
   def destroy
-end
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
   
   private
   def book_params
